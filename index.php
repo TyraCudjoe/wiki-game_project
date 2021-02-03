d6b405ee4fa25c12810060758cf13584eebb089a
tyra cudjoe <tyra20015@gmail.com>
2021-02-03 22:14:32 +0100

    playing around with different background colors

--- index.php
+++ index.php
@@ -16,6 +16,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com"> 
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Handlee&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@500&display=swap" rel="stylesheet">
 
   <title>Wiki GTA V</title>
 </head>
