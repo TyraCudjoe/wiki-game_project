92f164d5d95dc06c9ab027f444d76a7c618ba45c
tyra cudjoe <tyra20015@gmail.com>
2021-02-03 18:42:37 +0100

    used picture for banner

--- home.php
+++ home.php
@@ -1,3 +1,10 @@
 <div>
-    <img class="homeimg" src="./img/1582142840-screenshot-35501_1.jpeg.webp" alt="">
+    <div>
+        <img class="homeimg" src="./img/1582142840-screenshot-35501_1.png" alt="">
+    </div>
+    <div class="row">
+        <div class="col">
+
+        </div>
+    </div>
 </div>
