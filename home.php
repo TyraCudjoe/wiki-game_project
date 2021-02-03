a8b512ca40cde69cf992b6cdb13e4d3906619c6e
tyra cudjoe <tyra20015@gmail.com>
2021-02-03 20:03:42 +0100

    changed the navbar color + added a new font

--- home.php
+++ home.php
@@ -2,9 +2,15 @@
     <div>
         <img class="homeimg" src="./img/1582142840-screenshot-35501_1.png" alt="">
     </div>
-    <div class="row">
-        <div class="col">
-
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="display-6">Welcome to Wiki GTA V</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- index.php
+++ index.php
@@ -7,13 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="./css/style.css">
-  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-<link rel="icon" href="/favicon.ico" type="image/x-icon">
-  <link rel="icon" type="image/ico" sizes="16x16" href="./favicon.ico">
-  <link href="https://fonts.googleapis.com/css?family=Handlee&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./icon/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/ico" sizes="16x16" href="./favicon.ico">
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Handlee&display=swap" rel="stylesheet">
 
   <title>Wiki GTA V</title>
 </head>
