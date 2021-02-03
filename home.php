70accda32231716df561ce48209e17cd2feb3c36
AronVLD <aronridder@hotmail.nl>
2021-02-03 15:54:53 +0100

    fixed conflict

--- home.php
+++ home.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 <div>
     <img class="homeimg" src="./img/1582142840-screenshot-35501_1.jpeg.webp" alt="">
 </div>
-=======
-<h1>Grand theft auto Five</h1>
->>>>>>> e12c87afe65bf5cac40a74975364d4b561eab791
