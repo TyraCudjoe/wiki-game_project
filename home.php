3626beed32a00c3963246d8715fd4fe15a83a2c8
tyra cudjoe <tyra20015@gmail.com>
2021-02-04 14:43:26 +0100

    added 3 new cards and buttons + added hover attribute to

--- home.php
+++ home.php
@@ -1,20 +1,46 @@
 <div>
-<!-- djt voegt een plaatje toe -->
+    <!-- djt voegt een plaatje toe -->
     <div>
         <img class="homeimg" src="./img/1582142840-screenshot-35501_1.png" alt="">
     </div>
     <!-- maakt een container -->
     <div class="container-fluid">
         <div class="row">
-            <div class="col">
-            <!-- een card -->
+            <section class="col-12">
                 <div class="card">
                     <div class="card-body">
-                    <!-- text voor onder het plaatje -->
+                        <!-- text voor onder het plaatje -->
                         <h1 class="display-6">Welcome to Wiki GTA V</h1>
                     </div>
                 </div>
-            </div>
+            </section>
+            <section class="col-4 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <!-- text voor onder het plaatje -->
+                        <h2 class="">Micheal</h2>
+                    </div>
+                    <button type="button" class="btn btnMicheal">Primary</button>
+                </div>
+            </section>
+            <section class="col-4 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <!-- text voor onder het plaatje -->
+                        <h2 class="">Franklin</h2>
+                    </div>
+                    <button type="button" class="btn btnFranklin">Primary</button>
+                </div>
+            </section>
+            <section class="col-4 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <!-- text voor onder het plaatje -->
+                        <h2 class="">Trevor</h2>
+                    </div>
+                    <button type="button" class="btn btnTrevor">Primary</button>
+                </div>
+            </section>
         </div>
     </div>
 </div>
--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
     </a>
     <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
+    </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -18,9 +18,6 @@
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="#">Action</a></li>
                     <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
                     <li><a class="dropdown-item" href="#">Something else here</a></li>
                 </ul>
             </li>
