f53d0d3db1400daa663957d9a7eaa73e7cd21d78
AronVLD <aronridder@hotmail.nl>
2021-02-04 14:04:01 +0100

    change: add comment for content.php why: so its easier to understand

--- content.php
+++ content.php
@@ -1,3 +1,4 @@
+<!-- als de pagina opgeroepen word met content word er .php achter gezet anders gaat de pagina naar home.php   -->
 <?php
     if(isset($_GET["content"])){
         include("./" . $_GET["content"] . ".php");
