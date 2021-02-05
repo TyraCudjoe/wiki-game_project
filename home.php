87a031bfd21276110bfd9ef493ca048134a6f411
tyra cudjoe <tyra20015@gmail.com>
2021-02-05 14:04:27 +0100

    added 3 cards with 3 pictures

--- home.php
+++ home.php
@@ -40,7 +40,8 @@
                     <div class="card-body">
                         <!-- text voor onder het plaatje -->
                         <h2 class="">Franklin</h2>
-                        <p>Franklin Clinton is a fictional character and one of the three playable protagonists in Grand Theft Auto V, along with Michael De Santa and Trevor Philips. He is voiced by Shawn Fonteno.</p>
+                        <p>Franklin Clinton is a fictional character and one of the three playable protagonists in Grand Theft Auto V, along with Michael De Santa and Trevor Philips. He is voiced by Shawn Fonteno.
+                        </p>
                     </div>
                     <a href="./index.php?content=franklin.php">
                         <button type="button" class="btn btnFranklin">Click here for more.</button>
@@ -66,17 +67,49 @@
             </section>
             <section class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
                 <div class="card cardHover">
-                    <a href="./index.php?content=trevor.php">
-                        <img class="trevor" src="./img/trevor.jpg" alt="">
+                    <a href="./index.php?content=online.php">
+                        <img class="trevor" src="./img/gtavonline.jpg" alt="">
                     </a>
                     <div class="card-body">
                         <!-- text voor onder het plaatje -->
-                        <h2 class="">Trevor</h2>
-                        <p>Trevor Philips is a fictional character and one of the three playable protagonists, alongside Michael De Santa and Franklin Clinton, of Grand Theft Auto V, the seventh main title in the Grand Theft Auto series developed by Rockstar
-                            Games.
+                        <h2 class="">Online</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <a href="./index.php?content=online.php">
+                        <button type="button" class="btn btnTrevor">Click here for more.</button>
+                    </a>
+                </div>
+            </section>
+            <section class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div class="card cardHover">
+                    <a href="./index.php?content=speedrunner.php">
+                        <img class="trevor" src="./img/speedrun.jpg" alt="">
+                    </a>
+                    <div class="card-body">
+                        <!-- text voor onder het plaatje -->
+                        <h2 class="">Speedrunner</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <a href="./index.php?content=speedrunner.php">
+                        <button type="button" class="btn btnTrevor">Click here for more.</button>
+                    </a>
+                </div>
+            </section>
+            <section class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div class="card cardHover">
+                    <a href="./index.php?content=storymode.php">
+                        <img class="trevor" src="./img/storymode.jpg" alt="">
+                    </a>
+                    <div class="card-body">
+                        <!-- text voor onder het plaatje -->
+                        <h2 class="">Storymode</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
                     </div>
-                    <a href="./index.php?content=trevor.php">
+                    <a href="./index.php?content=storymode.php">
                         <button type="button" class="btn btnTrevor">Click here for more.</button>
                     </a>
                 </div>
