1644cdcbd80fc103e52e854c5d033239508c33b4
tyra cudjoe <tyra20015@gmail.com>
2021-02-05 15:02:44 +0100

    test

--- article.php
+++ article.php
@@ -1,32 +1,41 @@
 <?php 
-
+    // connects the datbase
     if (isset($_GET['name'])) {
         require_once 'connectdb.php';
         require_once 'functions.php';
         
+        //makes sure no one can hack the site
         $name = sanitize($_GET['name']);
-        
+
+        // selects all from name from the database
         $sql = "SELECT * FROM pages WHERE name='$name'";
         
+        // makes sure the mysql query comes trough
         $result = mysqli_query($conn, $sql);
 
+        
         $row = mysqli_fetch_assoc($result);
+
+        $records = "";
+        while ($record = mysqli_fetch_all($result)) {
+            $records .= "
+                        <a href='./index.php?content=update&name=". $record['name'] . "'>
+                            <img src='./img/b_edit.png' alt='pencil'>
+                        </a>";
+        }
 
         //var_dump($row);
 
     } else {
         echo 'doei';
     }
-
 ?>
 
 <div class="container info">
     <div class="card" id="infoCard">
         <div class="card-body">
             <h1 class="infoTitle"><?=$row['name']?> 
-            <a href='./index.php?content=update&id=". $record['name'] . "'>
-                 <img src='./img/b_edit.png' alt='pencil'>
-              </a>
+                <?php echo $records ?>
             <hr>
             <p><?=$row['info']?></p>
         </div>
--- update.php
+++ update.php
@@ -1,12 +1,22 @@
+<?php
+    $name = $_GET["name"];
 
+    include("./connectdb.php");
+    include("./functions.php");
+
+    $sql = "SELECT * FROM `pages` WHERE `name` = $name";
+    $result = mysqli_query($conn, $sql);
+    $record = mysqli_fetch_all($record);
+
+?>
 
 
 
 <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-  <input type="Text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  <label for="exampleFormControlInput1" class="form-label">Title</label>
+  <input type="Text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" >
 </div>
 <div class="mb-3">
   <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value="<?php echo$record[""] ?>"></textarea>
 </div>
